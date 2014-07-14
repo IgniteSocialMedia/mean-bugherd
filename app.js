e9f9785776dbbbd765c64e48859ef46b18c6b20e
Keith Morgan <keith.morgan@ignitesocialmedia.com>
2014-07-14 11:41:00 -0400

    revert

--- app.js
+++ app.js
@@ -11,15 +11,13 @@ MeanBugherd.register(function(app, auth, database) {
 
 	var template = fs.readFileSync(__dirname + '/template.js', 'utf8');
 
-	var bhId = mean.config.bugherdId;
+	var bhId = mean.config.clean.bugherdId;
 	
-	if(bhId){
-	    MeanBugherd.aggregateAsset('js', template.replace('__BUGHERD_ID__', bhId), {
+	MeanBugherd.aggregateAsset('js', template.replace('__BUGHERD_ID__', bhId), {
 		group: 'footer',
 		weight: -9999,
 		inline: true
-	    });
-	}
+	});
 
 	return MeanBugherd;
 });
